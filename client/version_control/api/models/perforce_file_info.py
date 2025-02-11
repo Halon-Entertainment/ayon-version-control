f16154eaff8efb929d4d267cdbb2f6c6ca33e2f0
Chad Provencher <thechad802@gmail.com>
2025-02-11 12:54:01 -0500

    created perforce file info

--- client/version_control/api/models/perforce_file_info.py
+++ client/version_control/api/models/perforce_file_info.py
@@ -3,9 +3,8 @@ import typing
 from dataclasses import dataclass, field
 
 from ayon_core.lib.log import Logger
-from ayon_core.pipeline.anatomy.anatomy import Anatomy
-from ayon_core.pipeline.template_data import get_template_data_with_names
-from ayon_core.settings.lib import get_project_settings
+from version_control.api.models.connection_info import ConnectionInfo
+from version_control.rest.perforce.rest_stub import PerforceRestStub
 
 log = Logger.get_logger(__name__)
 
@@ -15,20 +14,33 @@ class PerforceFileInfo:
     Represents a file in perforce.
     """
     
+    file_path: str
     depot_path: str
     revision_number: int
     workspace_path: str
     status: str
     changelist_number: int
-    exists:
-    
-    @classmethod
-    def from_p4_output(cls, output):
-        lines = output.strip().split('\n')
-        depot_path = lines[0].strip()
-        revision_number = int(lines[1].split(' ')[1])
-        workspace_path = lines[2].strip()
-        status = lines[3].strip()
-        changelist_number = int(lines[4].split(' ')[1])
-        
-        return cls(depot_path, revision_number, workspace_path, status, changelist_number)
+    connection_info: ConnectionInfo
+    exists: bool = field(default = False)
+
+
+
+    def __post_init__(self) -> None:
+        self.exists = self._get_file_exists()
+
+    def _get_file_exists(self) -> bool:
+        PerforceRestStub.login(
+            host=self.connection_info.workspace_server.host,
+            port=self.connection_info.workspace_server.port,
+            username=self.connection_info.workspace_server.username,
+            password=self.connection_info.workspace_server.password,
+            workspace_dir=self.connection_info.workspace_info.workspace_dir,
+            workspace_name=self.connection_info.workspace_info.workspace_name,
+        )
+        reponse = PerforceRestStub.exists_on_server(self.file_path)
+        return reponse['value']
+
+
+
+
+
